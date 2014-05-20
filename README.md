d27e439f3d2f7ebe78a5a60d06e1ee3e24a0b53f
Tobias Zeising <tobias.zeising@aditu.de>
2014-05-20 21:23:46 +0200

    new styling: * no mobile support * only tested on Google Chrome

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 php based bookmarks skript
 ==========================
 
-Copyright (c) 2013 Tobias Zeising, tobias.zeising@aditu.de  
+Copyright (c) 2014 Tobias Zeising, tobias.zeising@aditu.de  
 http://www.aditu.de
 Licensed under the GPLv3 license  
-Version 0.2
+Version 0.3
 
 This script shows all bookmarks from a browsers bookmarks.html (e.g. Chrome exported bookmarks).
 
 Bookmarks parser taken from http://www.phpclasses.org/browse.html/package/803.html
+
+Only tested with Google Chrome. No mobile support.
--- index.php
+++ index.php
@@ -1,148 +1,183 @@
 <?PHP
-	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-	header("Pragma: no-cache");
-	header("Cache-Control: no-store, no-cache, max-age=0, must-revalidate");
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Pragma: no-cache");
+    header("Cache-Control: no-store, no-cache, max-age=0, must-revalidate");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-	<title>bookmarks</title>
+    <title>bookmarks</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<link type="text/css" rel="stylesheet" media="screen, handheld, projection, tv" href="style.css" />
-    <script type="text/javascript" src="jquery-2.0.0.min.js"></script>
-	<script type="text/javascript">
-        jQuery(document).ready(function() {
-            $('h2').click(function() {
-                var urls = $(this).next();
-                if(urls.is(':visible'))
-                    urls.slideUp();
-                else
-                    urls.slideDown();
-            });
-            
-            $('#search-input input').keyup(function() {
-                var val = $(this).val().toLowerCase();
-                if(val.length < 2) {
-                    $('#search-results').html('');
-                    return;
-                }
-                
-                var links = "";
-                $('div.folder a').each(function(index, item) {
-                    if($(item).attr('title').toLowerCase().indexOf(val)!=-1) {
-                        links += $(item).parent().html();
-                    }
-                });
-                
-                $('#search-results').html('<ul>'+links+'</ul>');
-            });
-        });
-
-	</script>
+    <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
+    <link type="text/css" rel="stylesheet" media="screen, handheld, projection, tv" href="style.css" />
 </head>
 
 <body>
 
-<?php
+    <?php
 
-include ("BookmarkParser.php");
+        include ("BookmarkParser.php");
 
-$stack = array();
-$bookmarks = array();
+        $stack = array();
+        $bookmarks = array();
 
-function myURL($data, $depth, $no) {
-    global $bookmarks;
-    global $stack;
-    
-    // back to previous folder
-    while(count($stack)>$depth) {
-        array_pop($stack);
-    }
-    
-    $current = &$bookmarks;
-    foreach($stack as $name)
-        $current = &$current[$name];
-    $current[] = $data;
-}
-
-
-function myFolder($data, $depth, $no) {
-    global $bookmarks;
-    global $stack;
-    
-    $depth++;
-    
-    while(count($stack)>=$depth) {
-        array_pop($stack);
-    }
-    
-    $folder = $data["name"];
-    if(count($stack)==0 || $stack[count($stack)-1]!=$folder) {  
-        $current = &$bookmarks;
-        foreach($stack as $name)
-            $current = &$current[$name];
-        $current[$folder] = array();
-        array_push($stack, $folder);
-    }
-    
-}
-
-$class = new BookmarkParser();
-$class->parseNetscape("bookmarks.html", 0, 'myURL', 'myFolder');
-
-?>
-
-
-<div id="search-input">
-    <input type="text" />
-</div>
-<div id="search-results">
-
-</div>
-
-<?PHP
-
-function listFolder($folder) {
-    echo "<ul>";
-    
-    foreach($folder as $key => $data) {
-        echo "<li>";
-    
-        // folder
-        if(isset($data["url"])) {
-            $icon = $data["icon"];
-            if(strlen(trim($icon))==0)
-                $icon = "bookmark.png";
+        // will be executed by bookmarkparser for bookmarks
+        function myURL($data, $depth, $no) {
+            global $bookmarks;
+            global $stack;
             
-            if(strlen(trim($data["descr"]))>0)
-                echo '<a href="'.$data["url"].'" style="background-image:url('.$icon.')" title="'.$data["descr"].'" target="_blank">'.$data["descr"].'</a>';
+            // back to previous folder
+            while(count($stack)>$depth)
+                array_pop($stack);
             
-        // bookmark
-        } else {
-            echo "<h2>".$key."</h2>";
-            listFolder($data);
+            $current = &$bookmarks;
+            foreach($stack as $name)
+                $current = &$current[$name];
+            $current[] = $data;
         }
-        
-        echo "</li>";
-    }
-    echo "</ul>";
-}
 
+        // will be executed by bookmarkparser for folders
+        function myFolder($data, $depth, $no) {
+            global $bookmarks;
+            global $stack;
+            
+            $depth++;
+            
+            while(count($stack)>=$depth) {
+                array_pop($stack);
+            }
+            
+            $folder = $data["name"];
+            if(count($stack)==0 || $stack[count($stack)-1]!=$folder) {  
+                $current = &$bookmarks;
+                foreach($stack as $name)
+                    $current = &$current[$name];
+                $current[$folder] = array();
+                array_push($stack, $folder);
+            }
+            
+        }
 
-// Root Kategorien auflisten
-$color = 0;
-foreach($bookmarks as $key => $data) {
-    if(!isset($data["url"])) {
-        echo '<div class="folder">';
-        echo '<h1>'.$key.'</h1>';
-        listFolder($data);
-        echo "</div>";
-    }
-}
+        // parse bookmarks
+        $class = new BookmarkParser();
+        $class->parseNetscape("bookmarks.html", 0, 'myURL', 'myFolder');
 
-?>
+    ?>
 
+    <div id="wrapper">
+    
+        <!-- folder list -->
+        <div id="folders">
+            <!-- search box -->
+            <div id="search-input">
+                <input type="text" placeholder="search term" />
+            </div>
+
+            <?PHP
+                // list all bookmarks and recursive the subfolders with its bookmarks
+                function listFolder($folder) {
+                    // counts all bookmarks of current and subfolders
+                    $counter = 0;
+                    
+                    $html = "<ul>";
+                    
+                    foreach($folder as $key => $data) {
+                        
+                        // bookmark
+                        if(isset($data["url"])) {
+                            $counter++;
+                            
+                            $icon = $data["icon"];
+                            if(strlen(trim($icon))==0)
+                                $icon = 'bookmark.png';
+                            
+                            if(strlen(trim($data["descr"]))>0)
+                                $html = $html . '<li class="bookmark"><a href="'.$data["url"].'" style="background-image:url('.$icon.')" title="'.$data["descr"].'" target="_blank">'.$data["descr"].'</a></li>';
+                            
+                        // folder
+                        } else {
+                            $listResult = listFolder($data);
+                            $counter += $listResult["counter"];
+                            
+                            $html = $html . '<li>';
+                            $html = $html . '<h2>' . $key . ' <span>' . $listResult["counter"] . '</span></h2>';
+                            $html = $html . $listResult["html"];
+                            $html = $html . '</li>';
+                        }
+                    }
+                    
+                    $html = $html . "</ul>";
+                    return array("counter" => $counter,
+                                 "html" => $html);
+                }
+
+                $result = listFolder($bookmarks);
+                echo $result["html"];
+                $counter = $result["counter"];
+            ?>
+            
+            <div id="counter"><?PHP echo $counter; ?> bookmarks</div>
+        </div>
+
+        <!-- bookmarks -->
+        <ul id="bookmarks">
+            
+        </ul>
+    
+    </div>
+    
+    <!-- javascripts -->
+    <script type="text/javascript" src="jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function() {
+            // folder selection
+            $('h2').click(function() {
+                // set active
+                $(this).parent().parent().find('ul').slideUp();
+                $('#folders').find('.active').removeClass('active');
+                $(this).parent().addClass('active');
+                
+                // open submenue
+                var subelements = $(this).next();
+                if (subelements.find('li:not(.bookmark)').length>0)
+                    subelements.slideDown();
+                    
+                // show bookmarks
+                var urls = $(this).next().find('> li.bookmark');
+                $('#bookmarks').html('');
+                $('#bookmarks').append(urls.clone());
+            });
+            
+            // search
+            $('#search-input input').keyup(function() {
+                var val = $(this).val().toLowerCase();
+                if(val.length < 2) {
+                    $('#bookmarks').html('');
+                    return;
+                }
+                
+                $('#folders').find('.active').removeClass('active');
+                
+                $('#bookmarks').html('');
+                $('li.bookmark').each(function(index, item) {
+                    if($(item).find('a').attr('title').toLowerCase().indexOf(val)!=-1) {
+                        $('#bookmarks').append($(item).clone());
+                    }
+                });
+            });
+            
+            // set color
+            $('h2').each(function(index, item) {
+                var r = (Math.round(Math.random()* 127) + 127).toString(16);
+                var g = (Math.round(Math.random()* 127) + 127).toString(16);
+                var b = (Math.round(Math.random()* 127) + 127).toString(16);
+                $(this).css('borderLeft', '7px solid #' + r + g + b);
+            });
+
+        });
+
+    </script>
 
 </body>
 </html>
